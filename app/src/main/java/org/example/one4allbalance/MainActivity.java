75c4f77689670e15883c1948e0d00f59737add7e
Conor Flannery <cflannery91@gmail.com>
2015-01-28 21:35:00 +0000

    added button and postData() method running in onClick()

--- app/src/main/java/org/example/one4allbalance/MainActivity.java
+++ app/src/main/java/org/example/one4allbalance/MainActivity.java
@@ -2,9 +2,13 @@ package org.example.one4allbalance;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -13,18 +17,24 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(this);
     }
 
 
@@ -50,23 +60,44 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                postData();
+                break;
+            default:
+        }
+    }
+
     public void postData() {
         // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost("localhost/test/tester.php");
+        HttpPost httppost = new HttpPost("localhost:8080/test/tester.php");
+        HttpEntity entity;
+        String result;
 
         try {
             // Add your data
-            NameValuePair nameValuePair = new BasicNameValuePair("name", "12345");
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("name", "1234567"));
+            UrlEncodedFormEntity ent = new UrlEncodedFormEntity(nameValuePairs);
+            httppost.setEntity(ent);
 
             // Execute HTTP Post Request
+            // Execute HTTP Post Request
             HttpResponse response = httpclient.execute(httppost);
+            entity = response.getEntity();
+            //get xml result in string
+            result = EntityUtils.toString(entity);
+
+            Log.d("MainActivity", result);
+
 
         } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
+            e.printStackTrace();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
